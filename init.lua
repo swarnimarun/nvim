fc9fab62142006209d67dad5326d4b088ed7991e
Swarnim Arun <swarnimarun11@gmail.com>
2024-07-09 19:11:48 +0530

    fix: use on attach for rust

--- init.lua
+++ init.lua
@@ -21,16 +21,16 @@ vim.g.maplocalleader = " "
 -- setup config before plugins
 require("core.config")
 
-require("lazy").setup('plugins', {
+require("lazy").setup("plugins", {
     performance = {
         cache = {
             enabled = true,
         },
         reset_packpath = true, -- reset the package path to improve startup time
         rtp = {
-            reset = true,      -- reset the runtime path to $VIMRUNTIME and your config directory
+            reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
             ---@type string[]
-            paths = {},        -- add any custom paths here that you want to includes in the rtp
+            paths = {}, -- add any custom paths here that you want to includes in the rtp
             ---@type string[] list any plugins you want to disable here
             disabled_plugins = {
                 "gzip",
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,76 @@
+local on_attach = function(client, bufnr)
+    local keymap = vim.keymap -- for conciseness
+
+    local opts = { noremap = true, silent = true }
+    -- set keybinds for the entire buffer
+    opts.buffer = bufnr
+    vim.lsp.inlay_hint.enable(true)
+
+    opts.desc = "Go to declaration"
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    opts.desc = "Show LSP definitions"
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    opts.desc = "Show documentation for what is under cursor"
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    opts.desc = "Show LSP implementations"
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+
+    opts.desc = "Show signature help"
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+    opts.desc = "Add lsp workspace folder"
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+
+    opts.desc = "Remove lsp workspace folder"
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+
+    opts.desc = "List lsp workspace folders"
+    vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+
+    opts.desc = "Show LSP type definitions"
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+
+    opts.desc = "Smart rename"
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+
+    opts.desc = "See available code actions"
+    vim.keymap.set({ "n", "v" }, "<space>a", vim.lsp.buf.code_action, opts)
+
+    opts.desc = "Show LSP references"
+    vim.keymap.set("n", "gr", function()
+        require("telescope.builtin").lsp_references()
+    end, opts)
+
+    opts.desc = "Use LSP to format"
+    vim.keymap.set("n", "<space>f", function()
+        vim.lsp.buf.format({ async = true })
+    end, opts)
+
+    -- opts.desc = "Show buffer diagnostics"
+    -- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+    opts.desc = "Show line diagnostics"
+    keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+    opts.desc = "Go to previous diagnostic"
+    keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+    opts.desc = "Go to next diagnostic"
+    keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+    opts.desc = "Restart LSP"
+    keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+
+    -- Change the Diagnostic symbols in the sign column (gutter)
+    -- (not in youtube nvim video)
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+end
 return {
     {
         "neovim/nvim-lspconfig",
@@ -7,80 +80,6 @@ return {
             { "antosha417/nvim-lsp-file-operations", config = true },
         },
         config = function()
-            local on_attach = function(client, bufnr)
-                local keymap = vim.keymap -- for conciseness
-
-                local opts = { noremap = true, silent = true }
-                -- set keybinds for the entire buffer
-                opts.buffer = bufnr
-                vim.lsp.inlay_hint.enable()
-
-                opts.desc = "Go to declaration"
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                opts.desc = "Show LSP definitions"
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                opts.desc = "Show documentation for what is under cursor"
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                opts.desc = "Show LSP implementations"
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-
-                opts.desc = "Show signature help"
-                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-
-                opts.desc = "Add lsp workspace folder"
-                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-
-                opts.desc = "Remove lsp workspace folder"
-                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-
-                opts.desc = "List lsp workspace folders"
-                vim.keymap.set('n', '<space>wl', function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts)
-
-                opts.desc = "Show LSP type definitions"
-                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-
-                opts.desc = "Smart rename"
-                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-
-                opts.desc = "See available code actions"
-                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-
-                opts.desc = "Show LSP references"
-                vim.keymap.set('n', 'gr', function() require('telescope.builtin').lsp_references() end, opts)
-
-                opts.desc = "Use LSP to format"
-                vim.keymap.set('n', '<space>f', function()
-                    vim.lsp.buf.format { async = true }
-                end, opts)
-
-
-                -- opts.desc = "Show buffer diagnostics"
-                -- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-                opts.desc = "Show line diagnostics"
-                keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-                opts.desc = "Go to previous diagnostic"
-                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-                opts.desc = "Go to next diagnostic"
-                keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-
-                opts.desc = "Restart LSP"
-                keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-
-                -- Change the Diagnostic symbols in the sign column (gutter)
-                -- (not in youtube nvim video)
-                local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-                for type, icon in pairs(signs) do
-                    local hl = "DiagnosticSign" .. type
-                    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-                end
-            end
-
             -- import cmp-nvim-lsp plugin
             local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
@@ -89,6 +88,12 @@ return {
 
             -- import lspconfig plugin
             local lspconfig = require("lspconfig")
+
+            -- configure html server
+            lspconfig["kotlin_language_server"].setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
 
             -- configure html server
             lspconfig["html"].setup({
@@ -199,7 +204,7 @@ return {
                         NewLineForMembersInAnonymousTypes = false,
                         NewLineForClausesInQuery = false,
                     },
-                }
+                },
             })
 
             -- configure python server
@@ -227,7 +232,7 @@ return {
                         functionTypeParameters = true,
                         parameterNames = true,
                         rangeVariableTypes = true,
-                    }
+                    },
                 },
             })
 
@@ -256,34 +261,28 @@ return {
                         },
                         hint = {
                             enable = true,
-                        }
+                        },
                     },
                 },
             })
         end,
     },
     {
-        'mrcjkb/rustaceanvim',
-        version = '^4', -- Recommended
-        ft = { 'rust' },
+        "mrcjkb/rustaceanvim",
+        version = "^4", -- Recommended
+        ft = { "rust" },
         config = function()
             vim.g.rustaceanvim = {
                 -- Plugin configuration
                 -- tools = {},
                 -- LSP configuration
                 server = {
-                    on_attach = function(_, bufnr)
-                        -- you can also put keymaps in here
-                        vim.lsp.inlay_hint.enable()
-                        vim.keymap.set(
-                            "n",
-                            "<leader>a",
-                            function()
-                                vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
-                                -- or vim.lsp.buf.codeAction() if you don't want grouping.
-                            end,
-                            { silent = true, buffer = bufnr }
-                        )
+                    on_attach = function(c, b)
+                        vim.keymap.set("n", "<leader>a", function()
+                            vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+                            -- or vim.lsp.buf.codeAction() if you don't want grouping.
+                        end, { silent = true, buffer = b })
+                        on_attach(c, b)
                     end,
                     settings = {
                         -- rust-analyzer language server configuration
@@ -293,20 +292,20 @@ return {
                     dap = {},
                 },
             }
-        end
+        end,
     },
-    { 'b0o/SchemaStore.nvim', version = false },
+    { "b0o/SchemaStore.nvim", version = false },
     {
         "pmizio/typescript-tools.nvim",
         name = "typescript-tools",
         dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-        event = { 'BufReadPost *.ts,*.tsx,*.js,*.jsx', 'BufNewFile *.ts,*.tsx,*.js,*.jsx' },
+        event = { "BufReadPost *.ts,*.tsx,*.js,*.jsx", "BufNewFile *.ts,*.tsx,*.js,*.jsx" },
         opts = {},
         config = function()
             require("typescript-tools").setup({
                 settings = {
                     tsserver_file_preferences = {
-                        includeInlayParameterNameHints = 'all',
+                        includeInlayParameterNameHints = "all",
                         includeInlayParameterNameHintsWhenArgumentMatchesName = false,
                         includeInlayFunctionParameterTypeHints = true,
                         includeInlayVariableTypeHints = true,
@@ -315,12 +314,12 @@ return {
                     },
                 },
                 on_attach = function(_, bufnr)
-                    vim.api.nvim_set_hl(0, 'LspInlayHint', { fg = '#6272A4', italic = true })
-                    vim.lsp.inlay_hint.enable()
+                    vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#6272A4", italic = true })
+                    vim.lsp.inlay_hint.enable(true)
                 end,
             })
-        end
+        end,
     },
-    'ionide/Ionide-vim',
-    'p00f/clangd_extensions.nvim',
+    "ionide/Ionide-vim",
+    "p00f/clangd_extensions.nvim",
 }
