d10a92f4f9ef6113ef956d413d7755d74c8d6f9e
Swarnim Arun <swarnimarun11@gmail.com>
2024-11-09 23:11:34 +0530

    fixup

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -138,7 +138,7 @@ end
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if ! client then
+        if not client then
             return
         end
         on_attach_new(client, args.buf)
