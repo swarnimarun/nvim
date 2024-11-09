9d170426670c1abb70cb9b710741b9af0bc5373b
Swarnim Arun <swarnimarun11@gmail.com>
2024-11-09 23:26:33 +0530

    feat: go inlay hints

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -3,5 +3,18 @@ if vim.fn.executable('gopls') == 1 then
         name = 'gopls',
         cmd = { 'gopls' },
         root_dir = vim.fs.dirname(vim.fs.find({ 'go.mod' }, { upward = true })[1]),
+        settings = {
+            gopls = {
+                hints = {
+                    assignVariableTypes = true,
+                    compositeLiteralFiles = true,
+                    compositeLiteralTypes = true,
+                    constantValues = true,
+                    functionTypeParameters = true,
+                    parameterNames = true,
+                    rangeVariableTypes = true,
+                }
+            }
+        },
     })
 end
