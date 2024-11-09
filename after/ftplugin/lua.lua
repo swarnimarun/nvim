750414d3abaa0fb4c631d91921670ca7db23174f
Swarnim Arun <swarnimarun11@gmail.com>
2024-11-09 23:09:46 +0530

    feat: lua inlay hint & lsp keymap

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -5,6 +5,9 @@ if vim.fn.executable('lua-language-server') == 1 then
         root_dir = vim.fs.dirname(vim.fs.find({ 'init.lua', '*.lua' }, { upward = true })[1]),
         settings = {
             Lua = {
+                hint = {
+                    enable = true,
+                },
                 runtime = {
                     version = "LuaJIT",
                     pathStrict = true,
